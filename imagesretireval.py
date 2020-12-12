18835cb06c0dcd54f70db9bd7c9c7e2a07e78747
haneenalnahwi <haneenalnahwi@gmail.com>
2020-12-12 16:11:33 +0200

    saving histograms to CSV file

--- imagesretireval.py
+++ imagesretireval.py
@@ -1,73 +1,33 @@
-import numpy as np
 import cv2 as cv
-import os
-import matplotlib.pyplot as plt
-
-class Images:
-    def __init__(self, image, path):
-        self.path = path
-        self.image = image
-
-    def reSize(self):
-        img = self.image
-        img1 = cv.imread(img)
-        reSize = cv.resize(img1, (1400 * 1400))
-        return reSize
-
-    def RGBHistogram(self):
-        image_hight = self.image.shape[0]
-        image_width = self.image.shape[1]
-        image_channels = self.image.shape[2]
-        hist = np.zeros([256,image_channels], np.int)
-        for x in range(0, image_hight):
-            for y in range(0, image_width):
-                for c in range(0, image_channels):
-                    hist[self.image[x,y,c]] += 1
-        for i in range(0, hist.shape[0]):
-            for c in range(0,hist.shape[1]):
-                return i,c, hist[i,c]
+import numpy as np
+import glob
+import pandas as pd
 
 
-    def histSaving(self):
-        path = self.path
-        for file in os.listdir(path):
-            if os.path.isfile(path + file):
-                image = cv.imread(img)
+img_format = ['jpg', 'png', 'gif', 'jpeg']
+h = 0
+for frmt in img_format:
+    for img_name in glob.glob('*.' + frmt):
+        img = cv.imread(img_name)
+        hist_b = cv.calcHist([img], [0], None, [256], [0, 256])
+        hist_g = cv.calcHist([img], [1], None, [256], [0, 256])
+        hist_r = cv.calcHist([img], [2], None, [256], [0, 256])
 
-'''
+        hist_bn = np.zeros(256)
+        hist_gn = np.zeros(256)
+        hist_rn = np.zeros(256)
 
-def histogram(path):
-    # path = path
-    img = image
-    for infile in os.listdir(path):
-        if os.path.isfile(path + infile):
-            image = cv.imread(img)
-            file, ext = os.path.split(path + infile)
-            image.reshap(1400, 1400)
-            hist = cv.calcHist([image], [0], None, [256], [0, 256])
-            image.save(file + 'reshape.jpg' + 'JPEG')
-            data = np.loadtxt('data.txt')
-'''
+        for i in range(256):
+            hist_bn[i] = hist_b[i][0]
+            hist_gn[i] = hist_g[i][0]
+            hist_rn[i] = hist_r[i][0]
 
-
-def RGBHistogram(image):
-    img = cv.imread(image)
-    image_hight = img.shape[0]
-    image_width = img.shape[1]
-    image_channels = img.shape[2]
-    hist = np.zeros([256, image_channels], np.int)
-    for x in range(0, image_hight):
-        for y in range(0, image_width):
-            for c in range(0, image_channels):
-                hist[img[x, y, c]] += 1
-            for i in range(0, hist.shape[0]):
-                for c in range(0, hist.shape[1]):
-                    return i, c, hist[i, c]
-
-
-
-if __name__ == '__main__':
-    img = cv.imread('baby.jpg')
-    hist = cv.calcHist([img], [0], None, [256], [0, 256])
-    print(hist)
-
+        df = pd.DataFrame({'name': img_name,
+                            'Blue': hist_bn,
+                            'Green': hist_gn,
+                            'Red': hist_rn})
+        if h == 0:
+            df.to_csv('my_csv.csv', index=False)
+            h = 1
+        else:
+            df.to_csv('my_csv.csv', mode='a', header=False, index=False)
