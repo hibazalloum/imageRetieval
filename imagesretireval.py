07ff878504af12bd3982632c1f9969275b2eedab
hiba zalloum <hebahzalloum@gmail.com>
2020-12-18 19:55:29 +0200

    Arrange the code and write the comments

--- imagesretireval.py
+++ imagesretireval.py
@@ -3,82 +3,108 @@ import numpy as np
 import glob
 import pandas as pd
 
-def histograms(directory, name):
+
+def dataSetHistograms(path, name):
     img_format = ['jpg', 'png', 'jpeg']
+    # H : variable to save histogram for labels on cvs
     h = 0
-    hist_ = np.zeros((3,256,1))
+    hist = np.zeros((3, 256, 1))
     for frmt in img_format:
-        for img_address in glob.glob(directory + '*.' + frmt):      #using glob package to go to directory path
+        for img_address in glob.glob(path + '*.' + frmt):  # using glob package to go to directory path
             img = cv.imread(img_address)
-            img = cv.resize(img, (200,200))
-            img_name = img_address.replace(directory, '')
-            for x in range(3):
-                hist_[x] = cv.calcHist([img], [x], None, [256], [0, 256])
-            hist = cv.normalize(hist_, hist_)
+            img = cv.resize(img, (200, 200))
+
+            # to take image name without path
+            img_name = img_address.replace(path, '')
+
+            # calculate Histogram for BGR Color
+            for color in range(3):
+                hist[color] = cv.calcHist([img], [color], None, [256], [0, 256])
+            hist = cv.normalize(hist, hist)
             hist_b, hist_g, hist_r = np.zeros(256), np.zeros(256), np.zeros(256)
+
+            # To save data as a number not strings
             for i in range(256):
                 hist_b[i], hist_g[i], hist_r[i] = hist[0][i][0], hist[1][i][0], hist[2][i][0]
+
+            # save data as csv
             df = pd.DataFrame({'name': img_name, 'Blue': hist_b, 'Green': hist_g, 'Red': hist_r})
             if h == 0:
-                df.to_csv(directory + name + '.csv', index=False)
+                df.to_csv(path + name + '.csv', index=False)
                 h = 1
             else:
-                df.to_csv(directory + name + '.csv', mode='a', header=False, index=False)
-'''
-directory_ = 'C:\\Users\\hanee\\Desktop\\Master\\Python\\Project\\Dataset\\'
-name_ = 'dataset1'
-histograms(directory_, name_)
-'''
+                df.to_csv(path + name + '.csv', mode='a', header=False, index=False)
 
-dataset_hist = pd.read_csv('C:\\Users\\hanee\\Desktop\\Master\\Python\\Project\\Dataset\\dataset.csv')
 
-data_num = int(len(dataset_hist)/256)
-#print(data_num)
-#img = cv.imread('C:\\Users\\hanee\\Desktop\\Master\\Python\\Project\\Dataset\\2173.jpg')
-img = cv.imread('C:\\Users\\hanee\\Desktop\\Master\\Python\\Project\\johann_lurf_starfilm08.jpg')
-img = cv.resize(img, (200,200))
-hist_n = np.zeros((3,256,1))
-for c in range(3):
-    hist_n[c] = cv.calcHist([img], [c], None, [256], [0, 256])
-hist = cv.normalize(hist_n, hist_n)
-j = 0
-s = 0
-dist = np.zeros((data_num,3,256))
-for i in range(len(dataset_hist)+1):
-    if i > 0 and i % 256 == 0:
-        if s > (data_num): break
-        for k in range(1,4):
-            num = dataset_hist.iloc[j:i, k]
-            for l in range(j,i):
-                m = l-(256*(s))
-                dist[s][k-1][m] = abs(num[l] - hist[k-1][m][0])
-        j = i
-        s += 1
+def histQueryImage(path):
+    query_image = cv.imread(path)
+    query_image = cv.resize(query_image, (200, 200))
+    hist_n = np.zeros((3, 256, 1))
+    for c in range(3):
+        hist_n[c] = cv.calcHist([query_image], [c], None, [256], [0, 256])
+    hist = cv.normalize(hist_n, hist_n)
+    return hist
 
-distance = np.zeros(data_num)
 
-for x in range(data_num):
-    distance[x] = sum(dist[x][0]) + sum(dist[x][1]) + sum(dist[x][2])
+def distanceHistogram(dataNum, dataSetHist):
+    j = 0
+    pointer = 0
+    # create Array for Distances
+    dist = np.zeros((dataNum, 3, 256))  # number of image on the dataset
+    for i in range(len(dataset_hist) + 1):
+        if i > 0 and i % 256 == 0:  # To prevent redundancy 256 for one image ONLY ONE PERIOD
+            if pointer > (dataNum):
+                break
 
-indices = np.argsort(distance)
+            for colors in range(1, 4):
+                num = dataset_hist.iloc[j:i, colors]  # to take colors of image columns
+                for value in range(j, i):  # to take value of colors columns
+                    m = value - (256 * (pointer))
 
-images_10 = indices[:10]
-image_names = []
-names = dataset_hist.iloc[0:(len(dataset_hist)+1):256, 0]
-for z in range(data_num):
-    image_names.append(names[z*256])
+                    # save distance and differance between degree of colors and histogram image query
+                    dist[pointer][colors - 1][m] = abs(num[value] - histQuery[colors - 1][m][0])
+            j = i
+            pointer += 1
 
-img1_ = []
-img1 = []
-for im in images_10:
-    img1_.append('C:\\Users\\hanee\\Desktop\\Master\\Python\\Project\\Dataset\\' + image_names[im])
+    distance = np.zeros(dataNum)
+    # sum distance
+    for x in range(data_num):
+        distance[x] = sum(dist[x][0]) + sum(dist[x][1]) + sum(dist[x][2])
+    indices = np.argsort(distance)  # ascending order
+    return indices
 
-for h in range(10):
-    img1.append(cv.resize(cv.imread(img1_[h]),(200,200)))
 
-img_h1 = np.hstack((img1[0], img1[1], img1[2],img1[3], img1[4]))
-img_h2 = np.hstack((img1[5], img1[6], img1[7],img1[8], img1[9]))
-img_vh = np.vstack((img_h1,img_h2))
+def topTenImages(path):
+    #  takes name of the image once
+    image_names = []
+    names = dataset_hist.iloc[0:(len(dataset_hist) + 1):256, 0]
 
-cv.imshow('image1', img_vh)
-cv.waitKey(0)
+    # create new array Takes names of images with index = zero for number of image
+    for z in range(data_num):
+        image_names.append(names[z * 256])
+
+    # save the path of similar images to read and present them
+    img1_ = []
+    img1 = []
+    for im in top_ten:
+        img1_.append(path + image_names[im])
+    for h in range(10):
+        img1.append(cv.resize(cv.imread(img1_[h]), (200, 200)))
+
+    # to view result in one window!
+    img_h1 = np.hstack((img1[0], img1[1], img1[2], img1[3], img1[4]))
+    img_h2 = np.hstack((img1[5], img1[6], img1[7], img1[8], img1[9]))
+    img_vh = np.vstack((img_h1, img_h2))
+
+    cv.imshow('image1', img_vh)
+    cv.waitKey(0)
+
+
+if __name__ == '__main__':
+    dataSetHistograms('Dataset', 'dataset')
+    dataset_hist = pd.read_csv('Dataset/dataset.csv')
+    data_num = int(len(dataset_hist) / 256)
+    histQuery = histQueryImage('Dataset/2173.jpg')
+    distance = distanceHistogram(data_num, dataset_hist)
+    top_ten = distance[:10]
+    view_result = topTenImages('Dataset/')
